30feab515dcd69a8343ee0a4bac4193eee052bb6
chan9yu <wry5533@gmail.com>
2023-11-04 22:54:15 +0900

    feat: button 컴포넌트 구성 및 스토리 작성

--- src/components/common/Button/Button.stories.tsx
+++ src/components/common/Button/Button.stories.tsx
@@ -1,18 +1,124 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { Button } from '../../../components';
+import { Button, Flex } from '../../../components';
 
+/**
+ * 사용자의 상호작용을 위한 버튼 컴포넌트 입니다.
+ */
 const meta = {
 	title: 'Components/Common/Button',
 	component: Button,
-	tags: ['autodocs']
+	tags: ['autodocs'],
+	argTypes: {
+		children: { table: { disable: true } },
+		className: { table: { disable: true } },
+		style: { table: { disable: true } }
+	},
+	parameters: {
+		componentSubtitle: 'Button 컴포넌트'
+	},
+	decorators: [
+		Story => (
+			<Flex $alignItems="center" $gap={10}>
+				<Story />
+			</Flex>
+		)
+	]
 } satisfies Meta<typeof Button>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
+/**
+ * - Button 컴포넌트의 기본 상태입니다.
+ */
 export const Default: Story = {
 	args: {
 		children: 'Button'
 	}
+};
+
+/**
+ * - Theme Foundation에 정의한 `Color`를 사용하여 Button 컴포넌트의 색상을 변경할 수 있습니다.
+ */
+export const Color: Story = {
+	...Default,
+	render: args => (
+		<>
+			<Button {...args} $color="primary" />
+			<Button {...args} $color="secondary" />
+			<Button {...args} $color="gray" />
+			<Button {...args} $color="info" />
+			<Button {...args} $color="error" />
+			<Button {...args} $color="success" />
+			<Button {...args} $color="warning" />
+		</>
+	)
+};
+
+/**
+ * - 로딩 상태를 버튼 UI에 노출하기 위한 속성입니다.
+ */
+export const Loading: Story = {
+	...Default,
+	render: args => (
+		<>
+			<Button {...args} $loading $color="primary" />
+			<Button {...args} $loading $color="secondary" />
+			<Button {...args} $loading $color="gray" />
+			<Button {...args} $loading $color="info" />
+			<Button {...args} $loading $color="error" />
+			<Button {...args} $loading $color="success" />
+			<Button {...args} $loading $color="warning" />
+		</>
+	)
+};
+
+/**
+ * - 버튼을 비활성화 상태로 변경할 수 있는 속성입니다.
+ */
+export const Disabeld: Story = {
+	...Default,
+	render: args => (
+		<>
+			<Button {...args} disabled $color="primary" />
+			<Button {...args} disabled $color="secondary" />
+			<Button {...args} disabled $color="gray" />
+			<Button {...args} disabled $color="info" />
+			<Button {...args} disabled $color="error" />
+			<Button {...args} disabled $color="success" />
+			<Button {...args} disabled $color="warning" />
+		</>
+	)
+};
+
+/**
+ * - 버튼의 크기를 정의할 수 있는 속성입니다.
+ */
+export const Size: Story = {
+	...Default,
+	render: args => (
+		<>
+			<Button {...args} $size="sm" />
+			<Button {...args} $size="md" />
+			<Button {...args} $size="lg" />
+		</>
+	)
+};
+
+/**
+ * - Button 컴포넌트의 너비를 부모 요소에 기준으로 꽉 채웁니다.
+ */
+export const FullWidth: Story = {
+	...Default,
+	render: args => <Button {...args} $fullWidth />
+};
+
+/**
+ * - Button 컴포넌트의 너비를 설정합니다.
+ * - number만 입력하게 되면 자동으로 px 단위로 계산됩니다.
+ */
+export const Width: Story = {
+	...Default,
+	render: args => <Button {...args} $width={500} />
 };
--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,14 +1,40 @@
-import { ReactNode } from 'react';
+import { ButtonHTMLAttributes, forwardRef } from 'react';
+
+import { Loader } from '..';
+import type { CommonPropsWithChuldren } from '../../../@types';
 import * as S from './Button.styles';
 
-interface ButtonProps {
-	children?: ReactNode;
-}
+interface ButtonProps extends S.ButtonStyledProps, CommonPropsWithChuldren, ButtonHTMLAttributes<HTMLButtonElement> {}
 
-const Button = (props: ButtonProps) => {
-	const { children } = props;
+const Button = forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => {
+	const {
+		$color = 'primary',
+		$fullWidth = false,
+		$loading = false,
+		$size = 'md',
+		$width,
+		children,
+		className,
+		style,
+		...rest
+	} = props;
 
-	return <S.ButtonStyled>{children}</S.ButtonStyled>;
-};
+	const styledProps: S.ButtonStyledProps = {
+		$color,
+		$fullWidth,
+		$loading,
+		$size,
+		$width
+	};
+
+	return (
+		<S.ButtonStyled ref={ref} className={className} style={style} {...styledProps} {...rest}>
+			{$loading && <Loader $borderWidth="2px" $color={$color} $size={16} />}
+			{children}
+		</S.ButtonStyled>
+	);
+});
+
+Button.displayName = 'Button';
 
 export default Button;
