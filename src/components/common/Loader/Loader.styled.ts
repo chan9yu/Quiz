8f0a7949af70e2c45990cbb71da8d8ed0e0ab7c9
chan9yu <wry5533@gmail.com>
2023-11-04 21:18:05 +0900

    feat: loader 컴포넌트 구성 및 스토리 작성

--- src/components/common/Loader/Loader.styled.ts
+++ src/components/common/Loader/Loader.styled.ts
@@ -1,3 +1,31 @@
-import styled from 'styled-components';
+import { CSSProperties } from 'react';
+import styled, { DefaultTheme, css, keyframes } from 'styled-components';
 
-export const Wrapper = styled.div``;
+export interface LoaderStyledProps {
+	$color?: keyof DefaultTheme['color'];
+	$position?: CSSProperties['position'];
+	$size?: CSSProperties['width'];
+}
+
+const spinKeyframes = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+export const LoaderStyled = styled.div<LoaderStyledProps>`
+	border-radius: 50%;
+	animation: ${spinKeyframes} 1s linear infinite;
+
+	${props => css`
+		width: ${typeof props.$size === 'number' ? `${props.$size}px` : props.$size};
+		height: ${typeof props.$size === 'number' ? `${props.$size}px` : props.$size};
+		border: 5px solid ${props.theme.color.gray['100']};
+		border-bottom: 5px solid ${props.theme.color[props.$color || 'primary']['500']};
+		position: ${props.$position};
+	`}
+`;
--- src/components/common/Loader/Loader.tsx
+++ src/components/common/Loader/Loader.tsx
@@ -1,14 +1,22 @@
-import { ReactNode } from 'react';
+import { HTMLAttributes, forwardRef } from 'react';
+
+import type { CommonProps } from '../../../@types';
 import * as S from './Loader.styled';
 
-interface LoaderProps {
-	children?: ReactNode;
-}
+interface LoaderProps extends S.LoaderStyledProps, CommonProps, HTMLAttributes<HTMLDivElement> {}
 
-const Loader = (props: LoaderProps) => {
-	const { children } = props;
+const Loader = forwardRef<HTMLDivElement, LoaderProps>((props, ref) => {
+	const { $color = 'primary', $position = 'static', $size = '50px', className, style, ...rest } = props;
 
-	return <S.Wrapper>{children}</S.Wrapper>;
-};
+	const styledProps: S.LoaderStyledProps = {
+		$color,
+		$position,
+		$size
+	};
+
+	return <S.LoaderStyled ref={ref} className={className} style={style} {...styledProps} {...rest} />;
+});
+
+Loader.displayName = 'Loader';
 
 export default Loader;
