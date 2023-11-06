aa1b1e95d12b1b27d9f7ffa46db7a11a35d166d9
chan9yu <wry5533@gmail.com>
2023-11-06 16:41:44 +0900

    feat: choiceItem 컴포넌트 구현 및 스토리 작성

--- src/components/quiz/ChoiceItem/ChoiceItem.stories.tsx
+++ src/components/quiz/ChoiceItem/ChoiceItem.stories.tsx
@@ -1,18 +1,126 @@
 import type { Meta, StoryObj } from '@storybook/react';
+
+import { Flex } from '../..';
 import ChoiceItem from './ChoiceItem';
 
 const meta = {
 	title: 'Components/Quiz/ChoiceItem',
 	component: ChoiceItem,
-	tags: ['autodocs']
+	tags: ['autodocs'],
+	parameters: {
+		componentSubtitle: 'ChoiceItem 컴포넌트'
+	}
 } satisfies Meta<typeof ChoiceItem>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
+/**
+ * - ChoiceItem 컴포넌트의 기본 상태입니다.
+ */
 export const Default: Story = {
 	args: {
-		children: 'ChoiceItem'
+		children: '첫번째 지문입니다!',
+		prefixNumber: 1,
+		onClick: () => alert('click test')
 	}
+};
+
+/**
+ * - 퀴즈에 대한 지문이 4개인 상황입니다.
+ */
+export const Multiple: Story = {
+	...Default,
+	decorators: [
+		Story => (
+			<Flex $direction="column" $gap={12}>
+				<Story />
+			</Flex>
+		)
+	],
+	render: args => (
+		<>
+			<ChoiceItem {...args} prefixNumber={1}>
+				Javascript
+			</ChoiceItem>
+			<ChoiceItem {...args} prefixNumber={2}>
+				Python
+			</ChoiceItem>
+			<ChoiceItem {...args} prefixNumber={3}>
+				C#
+			</ChoiceItem>
+			<ChoiceItem {...args} prefixNumber={4}>
+				Java
+			</ChoiceItem>
+		</>
+	)
+};
+
+/**
+ * - 정답을 선택할 때
+ */
+export const Correct: Story = {
+	...Default,
+	args: {
+		...Default.args,
+		answerStatus: 'correct'
+	},
+	decorators: [
+		Story => (
+			<Flex $direction="column" $gap={12}>
+				<Story />
+			</Flex>
+		)
+	],
+	render: args => (
+		<>
+			<ChoiceItem {...args} prefixNumber={1} selected isCorrect>
+				Javascript
+			</ChoiceItem>
+			<ChoiceItem {...args} prefixNumber={2}>
+				Python
+			</ChoiceItem>
+			<ChoiceItem {...args} prefixNumber={3}>
+				C#
+			</ChoiceItem>
+			<ChoiceItem {...args} prefixNumber={4}>
+				Java
+			</ChoiceItem>
+		</>
+	)
+};
+
+/**
+ * - 오답을 선택할 때
+ */
+export const Incorrect: Story = {
+	...Default,
+	args: {
+		...Default.args,
+		answerStatus: 'incorrect'
+	},
+	decorators: [
+		Story => (
+			<Flex $direction="column" $gap={12}>
+				<Story />
+			</Flex>
+		)
+	],
+	render: args => (
+		<>
+			<ChoiceItem {...args} prefixNumber={1} isCorrect>
+				Javascript
+			</ChoiceItem>
+			<ChoiceItem {...args} prefixNumber={2} selected>
+				Python
+			</ChoiceItem>
+			<ChoiceItem {...args} prefixNumber={3}>
+				C#
+			</ChoiceItem>
+			<ChoiceItem {...args} prefixNumber={4}>
+				Java
+			</ChoiceItem>
+		</>
+	)
 };
--- src/components/quiz/ChoiceItem/ChoiceItem.tsx
+++ src/components/quiz/ChoiceItem/ChoiceItem.tsx
@@ -1,14 +1,35 @@
-import { ReactNode } from 'react';
+import { Text } from '../..';
+import type { ChildrenProps } from '../../../@types';
 import * as S from './ChoiceItem.styled';
 
-interface ChoiceItemProps {
-	children?: ReactNode;
+interface ChoiceItemProps extends ChildrenProps {
+	/** 현재 문제가 정답인지 오답인지 구분하는 데이터 */
+	answerStatus?: 'correct' | 'incorrect' | null;
+
+	/** isCorrect가 true면 정답인 지문 */
+	isCorrect?: boolean;
+
+	/** ChoiceItem 컴포넌트 클릭 이벤트 */
+	onClick?: () => void;
+
+	/** 몇 번 지문인지 UI에 노출하기 위해 필요한 데이터 */
+	prefixNumber?: number;
+
+	/** selected가 true면 내가 선택한 지문 */
+	selected?: boolean;
 }
 
 const ChoiceItem = (props: ChoiceItemProps) => {
-	const { children } = props;
+	const { answerStatus = null, children, isCorrect = false, onClick, prefixNumber, selected = false } = props;
 
-	return <S.Wrapper>{children}</S.Wrapper>;
+	return (
+		<S.Wrapper $answerStatus={answerStatus} $isCorrect={isCorrect} $selected={selected} onClick={onClick}>
+			<S.PrefixItem>{prefixNumber}</S.PrefixItem>
+			<Text $size="200" $weight="bold">
+				{children}
+			</Text>
+		</S.Wrapper>
+	);
 };
 
 export default ChoiceItem;
